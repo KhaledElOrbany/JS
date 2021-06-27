012b6800f76460de9fa74608cd6f17b9a48b4b58
Khaled <khaled.wagdy554@gmail.com>
2021-06-28 00:28:28 +0300

    Error handling & setting continuous working server

--- Node_API/app.js
+++ Node_API/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRouts = require('./api/routes/products');
 const ordersRouts = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productRouts);
 app.use('/orders', ordersRouts);
+
+//#region error_handling
+app.use((req, resp, next) => {
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, resp, next) => {
+    resp.status(error.status || 500);
+    resp.json({
+        err: {
+            msg: error.message
+        }
+    });
+});
+//#endregion
+
+
 
 module.exports = app;
